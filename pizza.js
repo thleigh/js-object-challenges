4afe534d0c71ad604427b055c80ec20b2fbaa4d3
Tanner Leigh <tannerhleigh@gmail.com>
2020-07-15 17:09:19 -0700

    [objects] add nest objects for pizza menu

---
+++ pizza.js
@@ -0,0 +1,24 @@
+const menu = {
+    cheese: {
+        toppings: ["sauce", "cheese", "garlic"],
+        price: 10
+    }
+    pepperoni: {
+        toppings: ["sauce", "cheese", "garlic"],
+        price: 10
+    }
+    ultimate: {
+        toppings: ["sauce", "cheese", "garlic"],
+        price: 10
+    }
+    vegan: {
+        toppings: ["sauce", "cheese", "garlic"],
+        price: 10
+    }
+    keto: {
+        toppings: ["sauce", "cheese", "garlic"],
+        price: 10
+    }
+}
+let pizzaMenu = object.keys(menu);
+console.log(pizzaMenu);
